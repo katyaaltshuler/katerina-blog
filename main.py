45d4eff9eccb2d7cd1785af3ba3950e420daf970
Katerina <katya.python.info@gmail.com>
2022-02-23 15:48:04 +0300

    Change sqlite to postgres

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ year = date.today().year
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = API_KEY
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
